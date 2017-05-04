32bdfdb5787e17107d510f26b6db74f54636b160
Halcao <nyz1500@gmail.com>
2017-05-05 03:25:48 +0800

    optimized

--- 01-LongestSubstringWithoutRepeating/solution.py
+++ 01-LongestSubstringWithoutRepeating/solution.py
@@ -1,18 +1,12 @@
-def isRepeating(s):
-    for c in s:
-        if len(str(s).split(c)) > 2:
-            return 1
-    return 0
-
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         n = len(s)
         m = min(n, 1)
         str = ""
         for i in range(0, n):
-            if not isRepeating(str+s[i]):
-                str += s[i]
-            else:
-               str = (str+s[i]).split(s[i])[1]+s[i]
+            str += s[i]
+            arr = str.split(s[i])
+            if len(arr) > 2:
+                str = arr[1]+s[i]
             m = max(len(str), m)
         return m
